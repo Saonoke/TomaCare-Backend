35015076b412ad97f7186997e319a81b203c2986
fighaz <sofisugiartozaini@gmail.com>
2024-11-05 19:50:40 +0700

    fix(community) : fix using session in repo community

--- database/repository/impl/post_repo.py
+++ database/repository/impl/post_repo.py
@@ -6,50 +6,46 @@ from model import Posts
 from database.database import engine
 
 class PostRepository(PostRepositoryMeta):
+    def __init__(self,session:Session):
+        self.session = session
     def get_all(self) -> List[Optional[Posts]]:
-        with Session(engine) as session:
-            statement = select(Posts)
-            results = session.exec(statement).all()
+        statement = select(Posts)
+        results = self.session.exec(statement).all()
         return results
     def get_by_id(self, _id: int) -> Posts:
-        with Session(engine) as session:
-            result = session.get(Posts, _id)
+        result = self.session.get(Posts, _id)
         return result
     def get_by_user_id(self, _user_id: int) -> List[Optional[Posts]] :
-        with Session(engine) as session:
-            statement = select(Posts).where(Posts.user_id == _user_id) 
-            results = session.exec(statement).all()
+        statement = select(Posts).where(Posts.user_id == _user_id) 
+        results = self.session.exec(statement).all()
         return results
     def add(self, model: Posts) -> Posts:
-        with Session(engine) as session:
-            session.add(model)
-            session.commit()
-            session.refresh(model)
+        self.session.add(model)
+        self.session.commit()
+        self.session.refresh(model)
         return model
     def edit(self, model: Posts, _id: int) -> Posts:
-        with Session(engine) as session:
-            statement = select(Posts).where(Posts.id == _id)
-            results = session.exec(statement)
-            db_post = results.one()
-            if db_post is None:
-                return None
+        statement = select(Posts).where(Posts.id == _id)
+        results = self.session.exec(statement)
+        db_post = results.one()
+        if db_post is None:
+            return None
 
-            db_post.title = model.title
-            db_post.body = model.body
-            db_post.image_id = model.image_id
+        db_post.title = model.title
+        db_post.body = model.body
+        db_post.image_id = model.image_id
 
-            session.commit()
-            session.refresh(db_post)
-            return db_post
+        self.session.commit()
+        self.session.refresh(db_post)
+        return db_post
     def delete(self, _id: int) -> bool  :
-        with Session(engine) as session:
-            statement = select(Posts).where(Posts.id == _id)
-            results = session.exec(statement)
-            db_post = results.one()
-            if db_post is None:
-                return None
-            session.delete(db_post)
-            session.commit()
-            return True
+        statement = select(Posts).where(Posts.id == _id)
+        results = self.session.exec(statement)
+        db_post = results.one()
+        if db_post is None:
+            return None
+        self.session.delete(db_post)
+        self.session.commit()
+        return True
 
     
