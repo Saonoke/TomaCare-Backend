6f4a96f41e3d7612b3aa4df887d1b5b0e06ebea8
Muhammad Naufal Kurniawan <naufalkurniawan605@gmail.com>
2024-11-13 17:03:25 +0700

    feat(posts-seeder): add some seeder data and implement argparse to seeder

--- database/seeder/user_seeder.py
+++ database/seeder/user_seeder.py
@@ -1,39 +1,32 @@
 from model import Users
 from sqlmodel import Session, select
 
+from utils import get_password_hash
+
 class users_seeder():
 
     def __init__(self,session:Session):
         self.session = session
 
-    def create_user(self):
-        user = [Users(id=1,email="krisnagmerz21@gmail.com",username="saonoke",full_name="krisna andika",password="tes")]
+    @staticmethod
+    def create_user():
+        user = [
+            Users(id=1,email="krisnagmerz21@gmail.com",username="saonoke",full_name="krisna andika",password=get_password_hash("tes")),
+            Users(id=2,email="naufalkurniawan605@gmail.com",username="nopal",full_name="Naufal Kurniawan",password=get_password_hash("Admin#1234")),
+            Users(id=3,email="figas@gmail.com",username="figas",full_name="Sofi",password=get_password_hash("Admin#1234")),
+        ]
         return user
-    
-    # def clear_user(self):
-    #     users = self.session.exec(select(Users)).all()
 
-    #     for user in users :
-    #         self.session.delete(user)
-    #         self.session.commit()
-        
-    
+    def clear(self):
+        users = self.session.exec(select(Users)).all()
+        for user in users :
+            self.session.delete(user)
+        self.session.commit()
+
+
     def execute(self):
-        # self.clear_user()
         users = self.create_user()
-        
+
         for user in users :
             self.session.add(user)
-            self.session.commit()
-
-
-
-        
-
-
-
-
-
-
-
-
+        self.session.commit()
