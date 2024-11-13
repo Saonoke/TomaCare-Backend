51e5858a93b5243287c15a2682cfb6ee529596de
SofiSugiharto Zaini <71657282+fighaz@users.noreply.github.com>
2024-11-13 16:25:23 +0700

    feat : add field in image model

--- model/images_model.py
+++ model/images_model.py
@@ -3,3 +3,4 @@ from sqlmodel import Field, SQLModel
 class Images(SQLModel, table=True):
     id: int = Field(primary_key=True, sa_column_kwargs={'autoincrement': True})
     image_path: str 
+    public_id : str | None = Field(default=None)
