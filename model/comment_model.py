5d9cf508f630cdeb53d756df65247fc1291fa58b
Muhammad Naufal Kurniawan <naufalkurniawan605@gmail.com>
2024-12-10 16:50:43 +0700

    fix(comunity-post): implement create timestamp in posts and comments

--- model/comment_model.py
+++ model/comment_model.py
@@ -1,12 +1,14 @@
 from sqlmodel import Field, Relationship, SQLModel
-
+import datetime
+from datetime import datetime
 
 class Comments(SQLModel, table=True):
     id: int = Field(primary_key=True, sa_column_kwargs={'autoincrement': True})
-    
+
     commentary : str
     post_id: int | None = Field(default=None, foreign_key="posts.id",ondelete="CASCADE")
     user_id: int | None = Field(default=None, foreign_key="users.id")
-    
+    created_at: datetime = Field(default_factory=lambda: datetime.now())
+
     user: "Users" = Relationship(back_populates="posts_comments")
     post: "Posts" = Relationship(back_populates="users_comments")
